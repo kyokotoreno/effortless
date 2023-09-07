05d79ff34fd6ddedc76ac24af991f142b5f19156
kyokotoreno <kyokotoreno@proton.me>
2023-09-06 23:55:13 -0500

    Fix Funcionalidad.java

--- effortless/resources/Funcionalidad.java
+++ effortless/resources/Funcionalidad.java
@@ -1,7 +1,12 @@
 package edu.usta.interfaces;
 
-import edu.usta.configuracion.MiConexion;
+import java.util.List;
 
 public interface Funcionalidad<T> {
-    
+    public Boolean registrar(T elObjeto);
+    public List<T> consultar(String orden);
+    T buscar(Integer llavePrimaria);
+    Boolean eliminar(Integer llavePrimaria);
+    Boolean actualizar(T elObjeto);
+    Integer totalRegistros();
 }
